91467fecac0e44750951379dd708b8359d0a6278
KmX <guydo.k@yandex.by>
2023-11-01 12:01:07 +0300

    Added triangulation

--- ACD.Logic/ModelDrawer/Renderer.cs
+++ ACD.Logic/ModelDrawer/Renderer.cs
@@ -49,7 +49,7 @@ public class Renderer : IRenderer
             }
         });
 
-        Span<Vector3Int> points = stackalloc Vector3Int[3];
+        Span<Vector3Int> points = stackalloc Vector3Int[_model.MaxPolygonVertices];
         Span<Line> lines = stackalloc Line[3];
         
         Span<Vector3Int> coords = stackalloc Vector3Int[2];
@@ -64,7 +64,7 @@ public class Renderer : IRenderer
             
             var baseIndex = pi * _model.MaxPolygonVertices;
 
-            for (var i = 0; i < 3; i++)
+            for (var i = 0; i < polygon.Vertices.Count; i++)
             {
                 points[i] = new Vector3Int(
                     (int)_vertices[baseIndex + i].X,
@@ -72,64 +72,67 @@ public class Renderer : IRenderer
                     (int)(_vertices[baseIndex + i].Z * 10_000_000));
             }
 
-            var maxY = Math.Max(Math.Max(points[0].Y, points[1].Y), points[2].Y);
-            var minY = Math.Min(Math.Min(points[0].Y, points[1].Y), points[2].Y);
-
-            maxY = Math.Clamp(maxY, 0, bitmap.Height - 1);
-            minY = Math.Clamp(minY, 0, bitmap.Height - 1);
-            
-            if (minY != maxY)
+            for (var i = 0; i < polygon.Vertices.Count - 2; i++)
             {
-                lines[0] = new Line(points[0], points[1]);
-                lines[1] = new Line(points[1], points[2]);
-                lines[2] = new Line(points[2], points[0]);
+                var maxY = Math.Max(Math.Max(points[0].Y, points[i + 1].Y), points[i + 2].Y);
+                var minY = Math.Min(Math.Min(points[0].Y, points[i + 1].Y), points[i + 2].Y);
+
+                maxY = Math.Clamp(maxY, 0, bitmap.Height - 1);
+                minY = Math.Clamp(minY, 0, bitmap.Height - 1);
                 
-                for (var y = maxY; y >= minY; y--)
+                if (minY != maxY)
                 {
-                    var c = 0;
+                    lines[0] = new Line(points[0], points[i + 1]);
+                    lines[1] = new Line(points[i + 1], points[i + 2]);
+                    lines[2] = new Line(points[i + 2], points[0]);
                     
-                    for (var i = 0; i < 3 && c < 2; i++)
+                    for (var y = maxY; y >= minY; y--)
                     {
-                        var line = lines[i];
-
-                        if (line.From.Y >= y && line.To.Y < y)
+                        var c = 0;
+                        
+                        for (var li = 0; li < 3 && c < 2; li++)
                         {
-                            var dy = y - line.From.Y;
-                            var dx = (line.To.X - line.From.X) * dy / (line.To.Y - line.From.Y);
-                            var dz = (line.To.Z - line.From.Z) * dy / (line.To.Y - line.From.Y);
+                            var line = lines[li];
 
-                            coords[c++] = new Vector3Int(line.From.X + dx, y, line.From.Z + dz);
-                        }
-                        else if (line.From.Y == line.To.Y && line.To.Y == y)
-                        {
-                            coords[0] = line.From;
-                            coords[1] = line.To;
-                            break;
-                        }
-                    }
-                    
-                    var from = Math.Clamp(coords[0].X, 0, bitmap.Width - 1);
-                    var to = Math.Clamp(coords[1].X, 0, bitmap.Width - 1);
+                            if (line.From.Y >= y && line.To.Y < y)
+                            {
+                                var dy = y - line.From.Y;
+                                var dx = (line.To.X - line.From.X) * dy / (line.To.Y - line.From.Y);
+                                var dz = (line.To.Z - line.From.Z) * dy / (line.To.Y - line.From.Y);
 
-                    if (from > to)
-                    {
-                        (from, to) = (to, from);
-                    }
-
-                    var z = coords[0].Z * 1f;
-                    var deltaZ = from == to ? 0f : (coords[1].Z - coords[0].Z + 0f) / (to - from);
-                    
-                    for (var x = from; x <= to; x++)
-                    {
-                        if (zBuffer[x, y] > z)
-                        {
-                            zBuffer[x, y] = (int)z;
-                            bitmap.DrawPixel(x, y, color);
+                                coords[c++] = new Vector3Int(line.From.X + dx, y, line.From.Z + dz);
+                            }
+                            else if (line.From.Y == line.To.Y && line.To.Y == y)
+                            {
+                                coords[0] = line.From;
+                                coords[1] = line.To;
+                                break;
+                            }
                         }
                         
-                        z += deltaZ;
+                        var from = Math.Clamp(coords[0].X, 0, bitmap.Width - 1);
+                        var to = Math.Clamp(coords[1].X, 0, bitmap.Width - 1);
+
+                        if (from > to)
+                        {
+                            (from, to) = (to, from);
+                        }
+
+                        var z = coords[0].Z * 1f;
+                        var deltaZ = from == to ? 0f : (coords[1].Z - coords[0].Z + 0f) / (to - from);
+                        
+                        for (var x = from; x <= to; x++)
+                        {
+                            if (zBuffer[x, y] > z)
+                            {
+                                zBuffer[x, y] = (int)z;
+                                bitmap.DrawPixel(x, y, color);
+                            }
+                            
+                            z += deltaZ;
+                        }
                     }
-                }
+                }   
             }
         }
     }
